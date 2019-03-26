f3692ddf4bd441c47bcd4595c32af253ca780285
Mehmet Perk <perkmehmed@gmail.com>
2019-03-26 20:38:10 +0300

    refactor %88 maxCounters

--- maxCounters.js
+++ maxCounters.js
@@ -1,13 +1,15 @@
 console.log(solution(5, [ 3, 4, 4, 6, 1, 4, 4]));
 function solution(N, A){
     var n = new Array(N).fill(0);
-    n = n.map(x => 0);
+    var max = 0;
     for (let k = 0; k < A.length; k++) {
         const x = A[k];
         if(1 <= x && x <= N){
             n[x-1] += 1;
+            if(n[x-1]>max){
+                max = n[x-1];
+            }
         } else {
-            var max = Math.max(...n);
             n.fill(max);
         }
     }
