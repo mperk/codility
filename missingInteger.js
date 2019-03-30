51e1bc8e5b32518d50d762c0a81fc22f6e0ee1cd
Mehmet Perk <perkmehmed@gmail.com>
2019-03-30 09:27:21 +0300

    refactor missingInteger. Score %100

--- missingInteger.js
+++ missingInteger.js
@@ -1,6 +1,26 @@
-console.log(solution([ 1, 3, 6, 4, 1, 2]));
+// This is a demo task.
+// Write a function:
+// class Solution { public int solution(int[] A); }
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+// Given A = [1, 2, 3], the function should return 4.
+// Given A = [−1, −3], the function should return 1.
+// Write an efficient algorithm for the following assumptions:
+// N is an integer within the range [1..100,000];
+
+
+// console.log(solution([ 1, 3, 6, 4, 1, 2]));
+console.log(solution([-1,-1, -5,-4,-2,-4,-8,-2,-3, 1]));
 function solution(A) {
-    // TODO: find best solution
+    const len = A.length;
+    const hash = {};
+    for (let i = 0; i < len; i++) {
+        hash[A[i]] = A[i];
+    }
+    for (let i = 1; i < 1000002; i++) {
+        if(!hash[i]) return i;
+    }
+    return 1;
 }
 
 
