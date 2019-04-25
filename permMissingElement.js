c187f588f765c95c06ad2aaf2337409662ad22ae
Mehmet Perk <perkmehmed@gmail.com>
2019-04-25 19:51:33 +0300

    Update permMissingElement.js
    refactor

--- permMissingElement.js
+++ permMissingElement.js
@@ -1,21 +1,5 @@
 console.log(bestSolution([2,3,1,5]));
 function solution(A){
-    var result = -1;
-    if(A.indexOf(1) < 0){
-        return 1;
-    }
-    for (let i = 0; i < A.length; i++) {
-        const element = A[i];
-        if(A.indexOf(element+1) < 0) {
-            if(result === -1 || result > element + 1) {
-                result = element + 1;
-            }
-        }
-    }
-    return result;
-}
-
-function bestSolution(A){
     var l = A.length + 1;
     var sum = (l * (l+1)) / 2;
     var sum2 = 0;
